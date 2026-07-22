041045b1671ae375b43bd5e7ec6fb6457f71ff27
AbingLeopoldo <pj.abing@gmail.com>
2026-07-22 23:36:44 +0800

    Connect Laravel and Vue using Vite

---
+++ resources/js/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container">
+        <h1>Expense Tracker</h1>
+        <p>Laravel 13 + Vue 3</p>
+        <p>By PJ</p>
+    </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.container {
+    padding: 40px;
+    font-family: Arial, Helvetica, sans-serif;
+}
+</style>
