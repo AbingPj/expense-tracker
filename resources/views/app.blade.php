ed90f5ef82e4beb012f82e79eb5f40660dca947b
AbingLeopoldo <pj.abing@gmail.com>
2026-07-24 22:56:34 +0800

    setup for tail wind css

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,15 +1,12 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Expense Tracker</title>
 
-    @vite('resources/js/app.js')
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
-
-<div id="app"></div>
-
+    <div id="app"></div>
 </body>
 </html>
