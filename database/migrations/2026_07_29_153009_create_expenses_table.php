46d887b58bef664156fd1dc53b625a319091b8de
AbingLeopoldo <pj.abing@gmail.com>
2026-07-30 00:37:34 +0800

    Update Expense migration

--- database/migrations/2026_07_29_153009_create_expenses_table.php
+++ database/migrations/2026_07_29_153009_create_expenses_table.php
@@ -11,8 +11,22 @@ return new class extends Migration
      */
     public function up(): void
     {
+        // Schema::create('expenses', function (Blueprint $table) {
+        //     $table->id();
+        //     $table->timestamps();
+        // });
+
         Schema::create('expenses', function (Blueprint $table) {
             $table->id();
+
+            $table->string('title');
+
+            $table->decimal('amount',10,2);
+
+            $table->date('expense_date');
+
+            $table->text('notes')->nullable();
+
             $table->timestamps();
         });
     }
