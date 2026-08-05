f628973adf5212d7a728fba67020b516c4d082b3
AbingLeopoldo <pj.abing@gmail.com>
2026-08-05 23:36:15 +0800

    added format function for the amount, and added action column with action buttons

--- resources/js/pages/Expenses.vue
+++ resources/js/pages/Expenses.vue
@@ -37,14 +37,41 @@
                             <th class="px-3 py-2 whitespace-nowrap">Title</th>
                             <th class="px-3 py-2 whitespace-nowrap">Amount</th>
                             <th class="px-3 py-2 whitespace-nowrap">Date</th>
+                            <th class="px-3 py-2 whitespace-nowrap">Action</th>
                         </tr>
                     </thead>
 
                     <tbody class="divide-y divide-gray-200">
                         <tr v-for="expense in expenses" :key="expense.id" class="*:text-gray-900 *:first:font-medium">
                             <td class="px-3 py-2 whitespace-nowrap">{{ expense.title }}</td>
-                            <td class="px-3 py-2 whitespace-nowrap">₱{{ expense.amount }}</td>
+                            <td class="px-3 py-2 whitespace-nowrap">₱ {{ formatAmount(expense.amount) }}</td>
                             <td class="px-3 py-2 whitespace-nowrap">{{ expense.expense_date }}</td>
+                            <td class="px-3 py-2 whitespace-nowrap">
+                                <div class="inline-flex">
+
+                                    <button
+                                        class="-ms-px border border-gray-200 px-3 py-2 text-gray-700 transition-colors hover:bg-gray-50 hover:text-gray-900 focus:z-10 focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:ring-offset-white focus:outline-none disabled:pointer-events-auto disabled:opacity-50 dark:border-gray-700 dark:text-gray-200 dark:hover:bg-gray-800 dark:hover:text-white dark:focus:ring-offset-gray-900"
+                                        aria-label="Edit"
+                                    >
+                                        <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
+                                        </svg>
+                                    </button>
+
+                                    <button
+                                        class="-ms-px rounded-e-sm border border-gray-200 px-3 py-2 text-gray-700 transition-colors hover:bg-gray-50 hover:text-gray-900 focus:z-10 focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:ring-offset-white focus:outline-none disabled:pointer-events-auto disabled:opacity-50 dark:border-gray-700 dark:text-gray-200 dark:hover:bg-gray-800 dark:hover:text-white dark:focus:ring-offset-gray-900"
+                                        aria-label="Delete"
+                                    >
+                                        <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
+                                            <path
+                                                stroke-linecap="round"
+                                                stroke-linejoin="round"
+                                                d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                                            />
+                                        </svg>
+                                    </button>
+                                </div>
+                            </td>
                         </tr>
                     </tbody>
                 </table>
@@ -101,5 +128,12 @@ const resetForm = () => {
     form.amount = "";
     form.expense_date = "";
     form.notes = "";
+};
+
+const formatAmount = (num) => {
+    if (num) {
+        num = Number(num);
+        return num.toLocaleString();
+    }
 };
 </script>
